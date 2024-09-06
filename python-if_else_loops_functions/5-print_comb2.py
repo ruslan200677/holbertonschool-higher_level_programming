8b699ae2387afd7801f00069df5e442070787cac
ruslan200677 <qaqli20033@gmail.com>
2024-09-06 17:48:38 +0400

    fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    end = ', ' if i < 99 else '\n'
-    print(f"{i:02d}", end=end)
-    print('')
+for i in range(0, 100):
+    if i == 99:
+        print(i)
+    else:
+        print("{:0>2d}".format(i), end=", ")
