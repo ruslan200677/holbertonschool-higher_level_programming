a2f4c307c424f2697c60e32a2f71025d14c44d0b
ruslan200677 <qaqli20033@gmail.com>
2024-09-07 18:51:25 +0000

    fix output

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
+for i in range(1, 100):
+    if i % 10 > i//10:
+        if i < 89:
+            print("{:02d}".format(i), end=', ')
         else:
-            print("{:d}{:d}".format(i, j), end="")
-            print(", ", end="")
+            print("{:02d}".format(i))
