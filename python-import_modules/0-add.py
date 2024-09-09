011d8c723735c18b917ae870e3c43e33c59e724d
ruslan200677 <qaqli20033@gmail.com>
2024-09-09 18:28:12 +0400

    1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,7 +4,7 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    result = add(a, b) 
+    result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
 
 if __name__ == "__main__":
