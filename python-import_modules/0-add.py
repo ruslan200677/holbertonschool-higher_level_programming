4215ea7134f32d4fbe1399e20b73c96297890d93
ruslan200677 <qaqli20033@gmail.com>
2024-09-09 18:21:59 +0400

    fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,7 +5,8 @@ def main():
     a = 1
     b = 2
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    # Use .format() method to format the output
+    print("{} + {} = {}".format(a, b, result))
 
 if __name__ == "__main__":
     main()
