19755a827976c691c7fcd4e26f1bc3d54e115dbe
ruslan200677 <qaqli20033@gmail.com>
2024-09-09 18:24:02 +0400

    style error

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
 
-def main():
+ def main():
     a = 1
     b = 2
     result = add(a, b)
     # Use .format() method to format the output
     print("{} + {} = {}".format(a, b, result))
 
-if __name__ == "__main__":
+ if __name__ == "__main__":
     main()
-
