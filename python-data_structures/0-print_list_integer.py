70be321e24f808412ff7c8f454c9a05b24dfbfab
ruslan200677 <qaqli20033@gmail.com>
2024-09-10 17:57:54 +0400

    test

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for element in my_list:
+        print("{}".format(element))
+
+
+if __name__ == '__main__':
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
