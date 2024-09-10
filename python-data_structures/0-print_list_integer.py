a6b325972bd502c41106dcfd82ce26d71e2c3fd2
ruslan200677 <qaqli20033@gmail.com>
2024-09-10 17:59:08 +0400

    f

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for element in my_list:
-        print("{}".format(element))
+        print("{:d}".format(element))
 
 
 if __name__ == '__main__':
