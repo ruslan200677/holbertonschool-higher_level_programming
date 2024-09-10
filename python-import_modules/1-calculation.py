6eb5d0d41f02878d197ba322565b037972ed9205
ruslan200677 <qaqli20033@gmail.com>
2024-09-10 16:08:49 +0400

    /

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,11 +6,8 @@ b = 5
 result = add(a, b)
 print("{} + {} = {}.format(a, b, result)
 result = sub(a, b)
-
 print("{} - {} = {}.format(a, b. result)
 result = mul(a, b)
-
 print("{} * {} = {}.format(a, b, result)
 result = div(a, b)
-
-print("{} : {} = {}.format(a, b, result)
+print("{} / {} = {}.format(a, b, result)
