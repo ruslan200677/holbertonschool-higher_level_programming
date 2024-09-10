c761bbc74f18f47ec693545639cb99f5cab4ac1b
ruslan200677 <qaqli20033@gmail.com>
2024-09-10 15:58:22 +0400

    fix

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -7,11 +7,9 @@ print("{} + {} = {}.format(a, b, result)
 result = sub(a, b)
 
 print("{} - {} = {}.format(a, b. result)
-result = sub(a, b)
-
-print("{} * {} = {}.format(a, b, result)
 result = mul(a, b)
 
-print("{} : {} = {}.format(a, b, result)
+print("{} * {} = {}.format(a, b, result)
 result = div(a, b)
 
+print("{} : {} = {}.format(a, b, result)
