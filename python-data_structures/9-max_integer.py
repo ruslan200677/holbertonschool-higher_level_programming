ee927313a98ec6bac4dd21a233de5c9f8b5691e1
ruslan200677 <qaqli20033@gmail.com>
2024-09-11 18:20:14 +0400

    add a file

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    largest = my_list[0]
+    for num in my_list:
+        if num > largest:
+            largest = num
+    return largest
