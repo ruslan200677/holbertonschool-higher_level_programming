ec59bf0ad6af2fcc5ce7ccd304fddc9fe8758e3e
ruslan200677 <qaqli20033@gmail.com>
2024-09-11 12:52:38 +0400

    probel

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-
-
 def element_at(my_list, idx):
   if idx < 0:
     return None
   if idx < 0 or idx >= len(my_list):
     return None
   return my_list[idx]
-
-
-if __name__ == "__main__":
-  my_list = [0, 2, 3]
-  
