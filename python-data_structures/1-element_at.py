1be8a6e4e2710016f267e43fe41d6bd7bde68e0b
ruslan200677 <qaqli20033@gmail.com>
2024-09-11 12:51:04 +0400

    isparvlenie owibki return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-   my_list = [0, 2, 3]
+
+
 def element_at(my_list, idx):
   if idx < 0:
     return None
   if idx < 0 or idx >= len(my_list):
     return None
-  print("{:d}".format(my_list, idx))
+  return my_list[idx]
+
+
+if __name__ == "__main__":
+  my_list = [0, 2, 3]
+  
