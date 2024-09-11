67547b3987a69fa5d5ff2fca0f0b1458d9d61afc
ruslan200677 <qaqli20033@gmail.com>
2024-09-11 17:53:54 +0400

    fix append

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,6 @@ def divisible_by_2(my_list=[]):
     new_list = ""
     for i in my_list:
         if i % 2 == 0:
-            new_list += True
+            new_list.append(True)
         else:
-            new_list += False
+            new_list.append(False)
