ca2444cbbaa7884e61f1b9de4af54e643cd1b468
ruslan200677 <qaqli20033@gmail.com>
2024-09-11 14:48:06 +0400

    string eror

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     new_string = []
     for i in my_string:
         if i != 'c' and i != 'C':
-            i += new_string
+            new_string += i
         return (new_string)
