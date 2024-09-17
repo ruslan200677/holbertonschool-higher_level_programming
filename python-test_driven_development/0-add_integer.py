f2feac3343f3b416393ad2c2799be7dd489349bb
ruslan200677 <qaqli20033@gmail.com>
2024-09-17 10:56:03 +0400

    FIX BAG

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-''' salammm   '''
+"""SALAM"""
+
 
 def add_integer(a, b=98):
-    ''' salam 2'''
+    """SALAM2"""
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
