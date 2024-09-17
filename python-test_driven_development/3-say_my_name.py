f8d8aeb1695c77975210e87262e33284c0456ef1
ruslan200677 <qaqli20033@gmail.com>
2024-09-17 11:58:23 +0400

    fix 2 if

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ Module contains function say my name """
-def say_my_name(first_name, last_name=""):
-    """ salammm """
-
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string and last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+def greet_person(first_name, last_name):
+    """Function to greet a person by their first and last names"""
+    
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    return f"Hello, {first_name} {last_name}!"
