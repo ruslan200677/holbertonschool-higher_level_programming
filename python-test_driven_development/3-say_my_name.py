486221d6ca35ac0cbe3dc18ce4c24d2c92ed8a7a
ruslan200677 <qaqli20033@gmail.com>
2024-09-17 11:51:45 +0400

    add a f ile

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+""" Module contains function say my name """
+def say_my_name(first_name, last_name=""):
+    """ salammm """
+
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string and last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
