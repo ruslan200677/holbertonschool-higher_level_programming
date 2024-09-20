a7d0cfe3767d5fac86067aa5d4ea4d59edc0c306
ruslan200677 <qaqli20033@gmail.com>
2024-09-20 20:36:23 +0400

    fix raise

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square():
     """Define class"""
     def __init__(self, size):
         if isinstance(size, int):
-            raise TypeError ("salamm")
+            raise TypeError ("size must be an integer")
         if size < 0:
-            raise ValueError("salma")
+            raise ValueError("size must be >= 0")
         self.__size = size
     pass
