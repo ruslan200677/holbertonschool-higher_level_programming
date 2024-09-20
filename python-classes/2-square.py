63107c49a45421b12493fe8ccb6a0ebef681c5b6
ruslan200677 <qaqli20033@gmail.com>
2024-09-20 20:39:03 +0400

    fix first if

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square():
     """Define class"""
     def __init__(self, size):
         """Initialize the square with a given size."""
-        if isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError ("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
