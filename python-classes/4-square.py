b2e4b9eed2f639c29b58cca4915e6a68ed0644bb
ruslan200677 <qaqli20033@gmail.com>
2024-09-21 15:20:36 +0400

    fix =0

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new Square"""
     @property
     def size(self):
