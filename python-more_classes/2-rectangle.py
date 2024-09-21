b49d69ecf8579ec94fe5ee2bbdd0095b321a8608
ruslan200677 <qaqli20033@gmail.com>
2024-09-21 18:38:18 +0400

    fix new line

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-
+        
     def area(self):
         return self.width * self.height
 
@@ -45,3 +45,4 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
+    
