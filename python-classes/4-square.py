52da7f273dc36c92b277208f09f19a297040cd1b
ruslan200677 <qaqli20033@gmail.com>
2024-09-21 15:19:43 +0400

    add a file 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square"""
+    @property
+    def size(self):
+        return(self.__size)
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    
+    def area(self):
+        ''' salammm'''
+        return(self.__size ** 2)
