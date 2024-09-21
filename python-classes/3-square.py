d7af13c303c30d8178587e2dfae6b2f99be6873e
ruslan200677 <qaqli20033@gmail.com>
2024-09-21 14:12:51 +0400

    fix pass2

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class that defines a square with a given size."""
     
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize the square with a given size."""
 
         
@@ -14,4 +14,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-pass
