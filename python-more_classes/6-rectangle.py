0ab66aec46fe5852b1212fe2cd275cace0570cb3
ruslan200677 <qaqli20033@gmail.com>
2024-09-21 18:05:38 +0000

    add

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Function for adding 2 integers"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Method to initialize the rectangle"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Method to get the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Method to set the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Method to get the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Method to set the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Method to compute the area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Method to compute the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Method to return the string representation of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join(["#" * self.width for i in range(self.height)])
+
+    def __repr__(self):
+        """Method to return the string representation of the rectangle"""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """Method to delete the rectangle"""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
