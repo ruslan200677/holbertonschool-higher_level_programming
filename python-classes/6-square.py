b0d1188a8d434340c0c4f1d1140383cc9481196e
ruslan200677 <qaqli20033@gmail.com>
2024-09-21 17:31:58 +0400

    ffx

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -35,11 +35,17 @@ class Square:
         self.__position = value
     
     def area(self):
-        ''' salammm'''
-        return(self.__size ** 2)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
     def my_print(self):
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
-        else:
-            for i in range(0, self.__size):
-                print("#" * self.__size)
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
