b6f58a0a2955f3ef7958eb0f51ce537d074fbf0e
ruslan200677 <qaqli20033@gmail.com>
2024-09-21 15:34:16 +0400

    add

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square"""
+        self.__size= size
+    @property
+    def size(self):
+        return(self.__size)
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    
+    def area(self):
+        ''' salammm'''
+        return(self.__size ** 2)
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(0, self.__size):
+                print("#" * self.__size)
