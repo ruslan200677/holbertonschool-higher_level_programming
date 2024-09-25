506d811aabdcddd267fc152874e5240cd36f64ed
ruslan200677 <qaqli20033@gmail.com>
2024-09-25 19:57:20 +0400

    fix retrun

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,3 +5,4 @@ def is_same_class(obj, a_class):
     '''Return True if obj is exactly an instance of a_class; otherwise False.'''
     if type(obj) is a_class:
         return True
+    return False
