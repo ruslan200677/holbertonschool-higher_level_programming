ca805acbeac1e78b5866f54c68a21f0ffd20f73b
ruslan200677 <qaqli20033@gmail.com>
2024-09-26 11:34:54 +0400

    add

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ func returns inherit"""
+
+    if is_kind_of_class(obj, a_class):
+        return True
+    else:
+        return False
