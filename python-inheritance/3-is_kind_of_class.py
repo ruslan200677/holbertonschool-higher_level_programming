c38140dd147e05a441aca37879f1780dada57425
ruslan200677 <qaqli20033@gmail.com>
2024-09-26 11:41:49 +0400

    fix

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,7 @@
 def is_kind_of_class(obj, a_class):
     """ func returns inherit"""
 
-    if is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
