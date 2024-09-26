a4ce945c132757284e8b37d188d9d49176a5daa4
ruslan200677 <qaqli20033@gmail.com>
2024-09-26 11:55:23 +0400

    jk

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ funnc rturns dir"""
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
