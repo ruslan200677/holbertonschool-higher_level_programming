3a21ffc938dc91be0c1f2392211d85bd40688b2d
ruslan200677 <qaqli20033@gmail.com>
2024-09-26 11:50:31 +0400

    add

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" function pyth3"""
+
+
+def inherits_from(obj, a_class):
+    """ funnc rturns dir"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
