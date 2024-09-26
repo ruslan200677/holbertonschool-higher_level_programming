c1539ba179abb56fb7bdd1a285fade8c9a6cec1c
ruslan200677 <qaqli20033@gmail.com>
2024-09-26 11:20:33 +0400

    fixx

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-''' usr bin snudiidq'''
+"""same class"""
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class; otherwise False."""
+    """checks if an object is instance of givem class"""
     if type(obj) is a_class:
         return True
     return False
