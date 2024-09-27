197ab39ef06b8d629f0c67504d31c5abfb0526a9
ruslan200677 <qaqli20033@gmail.com>
2024-09-27 10:19:58 +0400

    fix style

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Module containing BaseGeometry class"""
+"""Module containing sqaure class"""
 Rectangle = __import__(9-rectangle.py).Rectangle
 
 
 
 class Square(Rectangle):
-    """class rectangle with a parent basegeometry"""
+    """class square with a parent rectangle"""
     def __init__(self, width, height):
-        self.integer_validator('width', width)
-        self.__width = width
-        self.integer_validator('height', height)
-        self.__height = height
+        self.integer_validator('size', size)
+        self.__width = size
+        super().__init__(size, size)  
 
     def area(self):
-        return self.__width * self.__height
+        return self.__size * self.__size
