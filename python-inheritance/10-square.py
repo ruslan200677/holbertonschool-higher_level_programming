d108b8a7e86db2377655fdec6c42f6e644022375
ruslan200677 <qaqli20033@gmail.com>
2024-09-27 10:25:51 +0400

    fix ccom

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,7 +4,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """class"""
+    """classsqr rectangle impo"""
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
