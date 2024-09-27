732a466d70957c0c1878bf3fcf417ef9e256a056
ruslan200677 <qaqli20033@gmail.com>
2024-09-27 10:22:17 +0400

    fix '' in 3

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module containing sqaure class"""
-Rectangle = __import__(9-rectangle.py).Rectangle
+Rectangle = __import__('9-rectangle.py').Rectangle
 
 
 class Square(Rectangle):
