6fd60056c9e70e5b6efad17782ff4effe3cb5cbd
ruslan200677 <qaqli20033@gmail.com>
2024-09-27 11:18:09 +0400

    add a file-

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+from abc import ABC
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = abs(radius)
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        return pi * self.radius * 2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.height + self.width)
+
+def shape_info(obj):
+
+    area = obj.area()
+    perimeter = obj.perimeter()
+
+    print(f"Area: {area}")
+    print(f"Perimeter: {perimeter}")
