51ee9f5ba82a9829ffb2f5c9a08f59209a0089b5
ruslan200677 <qaqli20033@gmail.com>
2024-10-01 19:42:07 +0400

    add af ile

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" usr bin pyth"""
+
+
+def read_file(filename=""):
+    """def read"""
+    with open(filename="", "r", encoding='utf-8') as a file:
+        content = file.read()
+        print(contect, end="")
