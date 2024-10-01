59250b13b72ba9c8d9b6e9e242385843a8672e0c
ruslan200677 <qaqli20033@gmail.com>
2024-10-01 20:24:11 +0400

    fix doc+-

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""documn=ent"""
+"""module"""
 
 
 def to_json_string(my_obj):
-    """def to json to string and return a jsondumbs"""
+    """ the jekke"""
     import json
-    return json.dumbs(my_obj)
+    return json.dumps(my_obj)
