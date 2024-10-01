0398a2e8c28c05214cb0b7f24094e811aa684c68
ruslan200677 <qaqli20033@gmail.com>
2024-10-01 19:58:27 +0400

    adda file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""salam"""
+
+
+def append_write(filename="", text=""):
+    """sakmlmalsa"""
+    with open(filename, "a", endcoding="utf-18") as file:
+        return file.write(text)
