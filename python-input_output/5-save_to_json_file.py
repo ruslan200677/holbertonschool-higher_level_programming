259a447b4449ba037d8c4a213dc1a4bf6f44f2aa
ruslan200677 <qaqli20033@gmail.com>
2024-10-02 10:33:02 +0400

    fixx

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,5 @@
 
 def save_to_json_file(my_obj, filename):
     """function returns jsonnn"""
-    with open(filename, "w",encoding="utf=18") as file:
+    with open(filename, "w", encoding= "utf=18" )  as file:
         json.dump(my_obj, file)
