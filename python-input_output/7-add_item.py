54d78860cfc6a6024f56b23e09ddae5384b5ca54
ruslan200677 <qaqli20033@gmail.com>
2024-10-05 17:09:34 +0400

    sd

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """function"""
-import json
+
 
 
 def save_to_json_file(my_obj, filename):
     """JSON """
+    import json
     with open(filename, "w") as file:
         json.dump(my_obj, f)
