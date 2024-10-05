999df70791366439ecc34ba61302e26dea37f8e3
ruslan200677 <qaqli20033@gmail.com>
2024-10-05 17:10:38 +0400

    ew

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,8 @@
 
 def save_to_json_file(my_obj, filename):
     """JSON """
+def load_from_json_file(filename):
+    """sa,sla"""
     import json
     with open(filename, "w") as file:
         json.dump(my_obj, f)
