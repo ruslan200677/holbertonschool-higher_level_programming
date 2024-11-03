4b24f1ede587ad0ef94553ba02e1c17ac297bd49
ruslan200677 <qaqli20033@gmail.com>
2024-11-03 17:42:27 +0000

    fixx erorr

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""Module"""
+"""Cities by states"""
 
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb
 
     db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=argv[1],
         password=argv[2],
         database=argv[3]
     )
     cursor = db.cursor()
 
-    cursor.execute('SELECT c.id, c.name, s.name \
-                    FROM cities AS c \
-                    INNER JOIN states AS s ON s.id = c.state_id ')
+    cursor.execute(
+        'SELECT cities.id, cities.name, states.name '
+        'FROM cities '
+        'INNER JOIN states ON cities.state_id = states.id '
+        'ORDER BY cities.id'
+    )
 
-    for state in cursor.fetchall():
-        print(state)
+    for city in cursor.fetchall():
+        print(city)
 
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
+    cursor.close()
+    db.close()
