baaa6fe16d575f59a162745960747fd47a876f8f
ruslan200677 <qaqli20033@gmail.com>
2024-11-03 17:40:59 +0000

    fixx erorr

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""Module"""
+"""All cities by state"""
 
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb
 
     db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=argv[1],
         password=argv[2],
         database=argv[3]
     )
     cursor = db.cursor()
 
-    cursor.execute('SELECT c.name, s.name \
-                    FROM cities AS c \
-                    INNER JOIN states AS s ON s.id = c.state_id ')
+    cursor.execute(
+        'SELECT cities.id, cities.name, states.name '
+        'FROM cities '
+        'INNER JOIN states ON cities.state_id = states.id '
+        'ORDER BY cities.id'
+    )
 
-    cities = []
-    for city in cursor.fetchall():
-        if city[1] == argv[4]:
-            cities.append(city[0])
+    print(', '.join(c[1] for c in cursor.fetchall() if c[2] == argv[4]))
 
-    print(", ".join(cities))
-
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
+    cursor.close()
+    db.close()
