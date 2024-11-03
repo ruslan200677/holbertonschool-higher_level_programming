8eeba587adc4d9ab204c4480643dcc4fbf9807ba
ruslan200677 <qaqli20033@gmail.com>
2024-11-03 17:55:53 +0000

    isorav owibki

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-Module
-"""
+"""Get a state"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
-
 from model_state import Base, State
 
 if __name__ == "__main__":
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(argv[1], argv[2], argv[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
 
@@ -27,5 +24,4 @@ if __name__ == "__main__":
     else:
         print(state.id)
 
-    if session:
         session.close()
