4a6ba2eabcb6ab8b4ca7307e79092ad32e6edcb8
ruslan200677 <qaqli20033@gmail.com>
2024-11-03 16:52:26 +0000

    add

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-Connecting to database and listing it
-"""
+"""Filter states by user input"""
 
 if __name__ == "__main__":
-    import MySQLdb
     from sys import argv
+    import MySQLdb
 
     db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
-    mycursor = db.cursor()
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cursor = db.cursor()
 
     try:
-        query = """
-        SELECT * FROM states WHERE name='{:s}' ORDER BY states.id
-        """
-        mycursor.execute(query.format(argv[4]))
-        rows = mycursor.fetchall()
+        query = """SELECT * FROM states WHERE name='{:s}' ORDER BY states.id"""
+        cursor.execute(query.format(argv[4]))
+        rows = cursor.fetchall()
     except MySQLdb.Error as e:
         print(e)
 
@@ -29,5 +25,5 @@ if __name__ == "__main__":
         if row[1] == argv[4]:
             print(row)
 
-    mycursor.close()
+    cursor.close()
     db.close()
